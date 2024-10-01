a00795c1d1c10fbbf4917a4ab777142de6c661ce
rhian1543 <rhealynpinasahil@gamil.com>
2024-10-01 11:19:46 +0800

    lesson3

---
+++ task1.py
@@ -0,0 +1,13 @@
+favfruits = int(input("enter number of fruit :"))
+namesOffruits = []
+
+for i in range(favfruits):
+    name = input("enter name of fruit :")
+    nameOffruit.append(name)
+    if nameOffruits == "apple":
+       print("happy Eating")
+    elif nameOffruits == "banana":
+        break
+    
+print(namesoffruits)
+
